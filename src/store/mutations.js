f77de62ad283c8cdf95e736b0c5cdb79475d69aa
Maurizio Brandonisio <m.brandonisio@frankhood.it>
2021-05-03 21:32:59 +0200

    v0.2.5 Fix on Safari when the scrollbar is a normal one and not a overlay one, still doesn't work on page load but only when a resize is triggered, because on page load Safari doesn't account the sidebar width in the clientWidth

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,7 +2,12 @@ import * as types from './mutation_types';
 
 export default {
   [types.SET_VIEWPORT_SIZE](state) {
-    state.$viewportWidth = window.innerWidth;
+    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) && window.scrollbars.visible && window.innerWidth > document.body.clientWidth) {
+      /* Fix on Safari when the scrollbar is a normal one and not a overlay one, still doesn't work on page load but only when a resize is triggered, because on page load Safari doesn't account the sidebar width in the clientWidth */
+      state.$viewportWidth = document.body.clientWidth;
+    } else {
+      state.$viewportWidth = window.innerWidth;
+    }
     document.documentElement.style.setProperty('--vw', `${state.$viewportWidth / 100}px`);
     state.$viewportHeight = window.innerHeight;
     document.documentElement.style.setProperty('--vh', `${state.$viewportHeight / 100}px`);
